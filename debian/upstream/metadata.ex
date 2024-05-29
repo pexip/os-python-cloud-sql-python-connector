326923348f745fed17118e90532503e5cda4ee79
Huw Jones <huw@pexip.com>
2024-05-29 10:22:35 +0100

    Prepare Pexip packaging.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/python-cloud-sql-python-connector/issues
+# Bug-Submit: https://github.com/<user>/python-cloud-sql-python-connector/issues/new
+# Changelog: https://github.com/<user>/python-cloud-sql-python-connector/blob/master/CHANGES
+# Documentation: https://github.com/<user>/python-cloud-sql-python-connector/wiki
+# Repository-Browse: https://github.com/<user>/python-cloud-sql-python-connector
+# Repository: https://github.com/<user>/python-cloud-sql-python-connector.git
